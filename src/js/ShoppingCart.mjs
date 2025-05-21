9c72211e65d999f8c3181483416f8f6c5a416095
douglasgreyling <greyling.douglas@gmail.com>
2025-05-21 18:30:42 +0200

    Implement dynamic footer + header partials and stretch activity

---
+++ src/js/ShoppingCart.mjs
@@ -0,0 +1,34 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+export default class ShoppingCart {
+  constructor(dataSource, listElement) {
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+
+  async init() {
+    const list = this.dataSource;
+    this.renderList(list);
+  }
+
+  renderList(list) {
+    renderListWithTemplate(cartItemTemplate, this.listElement, list);
+  }
+}
+
+function cartItemTemplate(item) {
+  return `<li class="cart-card divider">
+    <a href="#" class="cart-card__image">
+      <img
+        src="${item.Image}"
+        alt="${item.Name}"
+      />
+    </a>
+    <a href="#">
+      <h2 class="card__name">${item.Name}</h2>
+    </a>
+    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+    <p class="cart-card__quantity">qty: 1</p>
+    <p class="cart-card__price">$${item.FinalPrice}</p>
+  </li>`;
+}
